379f4c5511873448a924c9e91b7302d7e2d1b4f7
Roman Vynar <roman.vynar@percona.com>
2016-06-02 21:29:10 +0300

    Renamed job linux>os. Nginx autorestart false.

---
+++ docker-push.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -eu
+
+VER="$(cat VERSION)"
+REV="$(git log -n 1 --no-walk --pretty="%h")"
+DATE="$(TZ="UTC" date "+%Y%m%d")"
+
+docker tag percona/pmm-server percona/pmm-server:"$VER-dev$DATE.$REV"
+docker push percona/pmm-server:"$VER-dev$DATE.$REV"
