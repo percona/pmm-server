7a562d98b3fa1229e18646d5be602d8ed44ccd0e
Vadim Yalovets <vadim.yalovets@percona.com>
2018-02-23 14:36:28 +0200

    PMM-2109 Set Home page to home-dashboard

--- import-dashboards.py
+++ import-dashboards.py
@@ -224,7 +224,7 @@ def set_home_dashboard(api_key):
     # Get dashboard information by dashboard slug (name) which is "home" in our case
     # This API is different from /api/dashboards/home which returns home dashboard
     # TODO replace "system-overview" with "home"
-    r = requests.get('%s/api/dashboards/db/system-overview' % (HOST,), headers=grafana_headers(api_key))
+    r = requests.get('%s/api/dashboards/db/home-dashboard' % (HOST,), headers=grafana_headers(api_key))
     print ' * "home" dashboard: %r %r' % (r.status_code, r.content)
     if r.status_code != 200:
         # TODO sys.exit(-1)
