8d721dd08707f2418c70d78a4ff10c531fe69e79
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2018-02-23 12:30:04 +0300

    PMM-2109 Use system-overview dashboard for test.

--- import-dashboards.py
+++ import-dashboards.py
@@ -223,7 +223,8 @@ def import_apps(api_key):
 def set_home_dashboard(api_key):
     # Get dashboard information by dashboard slug (name) which is "home" in our case
     # This API is different from /api/dashboards/home which returns home dashboard
-    r = requests.get('%s/api/dashboards/db/home' % (HOST,), headers=grafana_headers(api_key))
+    # TODO replace "system-overview" with "home"
+    r = requests.get('%s/api/dashboards/db/system-overview' % (HOST,), headers=grafana_headers(api_key))
     print ' * "home" dashboard: %r %r' % (r.status_code, r.content)
     if r.status_code != 200:
         # TODO sys.exit(-1)
