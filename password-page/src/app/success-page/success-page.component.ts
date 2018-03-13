08ff197b1133e00bc985fb80853c4fc463f48667
Daria Lymanska <daria.lymanska@gmail.com>
2018-03-13 16:50:52 +0200

    [PMM-1823] Update success page and add automatic redirect to main page

--- password-page/src/app/success-page/success-page.component.ts
+++ password-page/src/app/success-page/success-page.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-success-page',
-  templateUrl: './success-page.component.html',
-  styleUrls: ['./success-page.component.scss']
+    selector: 'app-success-page',
+    templateUrl: './success-page.component.html',
+    styleUrls: ['./success-page.component.scss']
 })
 export class AppSuccessPageComponent implements OnInit {
 
-  constructor() { }
+    constructor(private router: Router) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.redirectToMainPage();
+    }
 
+    /**
+     * Automatic redirect to installation page
+     */
+    public redirectToMainPage(): void {
+        setTimeout(() => {
+            this.router.navigate(['../installation-type']);
+        }, 5000);
+    }
 }
