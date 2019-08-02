f340462f308b07ea3bd7a4dae52ad2915850ee72
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2019-08-02 14:49:17 +0300

    PMM-3670 Remove old seds.

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,41 +8,6 @@ if [ -n "${ENABLE_DEBUG}" ]; then
     exec > >(tee -a /var/log/$(basename $0).log) 2>&1
 fi
 
-# Prometheus
-if [[ ! "${METRICS_RESOLUTION:-1s}" =~ ^[1-5]s$ ]]; then
-    echo "METRICS_RESOLUTION takes only values from 1s to 5s."
-    exit 1
-fi
-sed "s/1s/${METRICS_RESOLUTION:-1s}/" /etc/prometheus.yml > /tmp/prometheus.yml
-cat /tmp/prometheus.yml > /etc/prometheus.yml
-rm -rf /tmp/prometheus.yml
-
-sed "s/ENV_METRICS_RETENTION/${METRICS_RETENTION:-720h}/" /etc/supervisord.d/pmm.ini > /tmp/pmm.ini
-sed -i "s/ENV_MAX_CONNECTIONS/${MAX_CONNECTIONS:-15}/" /tmp/pmm.ini
-
-if [ -n "$METRICS_MEMORY" ]; then
-    # Preserve compatibility with existing METRICS_MEMORY variable.
-    # https://jira.percona.com/browse/PMM-969
-    METRICS_MEMORY_MULTIPLIED=$(( ${METRICS_MEMORY} * 1024 ))
-else
-    MEMORY_LIMIT=$(cat /sys/fs/cgroup/memory/memory.limit_in_bytes || :)
-    TOTAL_MEMORY=$(( $(grep MemTotal /proc/meminfo | awk '{print$2}') * 1024 ))
-    MEMORY_AVAIABLE=$(printf "%i\n%i\n" "$MEMORY_LIMIT" "$TOTAL_MEMORY" | sort -n | grep -v "^0$" | head -1)
-    METRICS_MEMORY_MULTIPLIED=$(( (${MEMORY_AVAIABLE} - 256*1024*1024) / 100 * 15 ))
-    if [[ $METRICS_MEMORY_MULTIPLIED -lt $((128*1024*1024)) ]]; then
-        METRICS_MEMORY_MULTIPLIED=$((128*1024*1024))
-    fi
-fi
-sed -i "s/ENV_METRICS_MEMORY_MULTIPLIED/${METRICS_MEMORY_MULTIPLIED}/" /tmp/pmm.ini
-
-cat /tmp/pmm.ini > /etc/supervisord.d/pmm.ini
-rm -rf /tmp/pmm.ini
-
-# Cron
-sed "s/^INTERVAL=.*/INTERVAL=${QUERIES_RETENTION:-8}/" /etc/cron.daily/purge-qan-data > /tmp/purge-qan-data
-cat /tmp/purge-qan-data > /etc/cron.daily/purge-qan-data
-rm -rf /tmp/purge-qan-data
-
 # Upgrade
 if [ -f /var/lib/grafana/grafana.db ]; then
     chown -R pmm:pmm /srv/prometheus/data
