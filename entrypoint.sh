444c8ac9cab325fd8d3a99b01a53ef220ffe06e5
Viacheslav Sarzhan <slava.sarzhan@percona.com>
2019-09-03 11:29:11 +0300

    PMM-4577 Cleanup Docker entrypoint script (#162)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,32 +2,5 @@
 
 set -o errexit
 
-# Add logging
-if [ -n "${ENABLE_DEBUG}" ]; then
-    set -o xtrace
-    exec > >(tee -a /var/log/$(basename $0).log) 2>&1
-fi
-
-# Upgrade
-if [ -f /var/lib/grafana/grafana.db ]; then
-    chown -R pmm:pmm /srv/prometheus/data
-    chown -R grafana:grafana /var/lib/grafana
-fi
-
-# copy SSL, follow links
-pushd /etc/nginx >/dev/null
-    if [ -s ssl/server.crt ]; then
-        cat ssl/server.crt  > /srv/nginx/certificate.crt
-    fi
-    if [ -s ssl/server.key ]; then
-        cat ssl/server.key  > /srv/nginx/certificate.key
-    fi
-    if [ -s ssl/dhparam.pem ]; then
-        cat ssl/dhparam.pem > /srv/nginx/dhparam.pem
-    fi
-popd >/dev/null
-
 # Start supervisor in foreground
-if [ -z "${UPDATE_MODE}" ]; then
-    exec supervisord -n -c /etc/supervisord.conf
-fi
+exec supervisord -n -c /etc/supervisord.conf
