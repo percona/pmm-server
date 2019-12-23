c6ee581ad4f683b295975b8d298719c8b9addc5a
Roman Misyurin <lunaticusgreen@users.noreply.github.com>
2019-12-23 23:35:35 +0300

    PMM-5201 Add correct doc link. (#187)

--- installation-wizard-page/src/App.jsx
+++ installation-wizard-page/src/App.jsx
@@ -1,31 +1,31 @@
-import React, { useState } from "react";
-import { Col, notification, Row } from "antd";
-import axios from "axios";
-import "./App.css";
-import "antd/dist/antd.css";
-import Button from "antd/es/button";
-import logo from "./assets/pmm-logo.png";
+import React, { useState } from 'react';
+import { Col, notification, Row } from 'antd';
+import axios from 'axios';
+import './App.css';
+import 'antd/dist/antd.css';
+import Button from 'antd/es/button';
+import logo from './assets/pmm-logo.png';
 
 function App() {
-  const [instanceId, setInstanceId] = useState("");
+  const [instanceId, setInstanceId] = useState('');
   const [loading, setLoading] = useState(false);
   const checkInstance = async () => {
     setLoading(true);
 
     try {
-      await axios.post("/v1/AWSInstanceCheck", {
+      await axios.post('/v1/AWSInstanceCheck', {
         instance_id: instanceId.trim()
       });
       // eslint-disable-next-line no-undef
-      window.location.href = "/";
+      window.location.href = '/';
     } catch (error) {
       notification.open({
         message: error.response.data.message
           ? error.response.data.message
           : error.message,
         style: {
-          backgroundImage: "linear-gradient(90deg,#e02f44,#c4162a)",
-          color: "white"
+          backgroundImage: 'linear-gradient(90deg,#e02f44,#c4162a)',
+          color: 'white'
         }
       });
     }
@@ -33,23 +33,23 @@ function App() {
   };
 
   return (
-    <div className="instance-id-form">
-      <div className="container-fluid header">
+    <div className='instance-id-form'>
+      <div className='container-fluid header'>
         <Row>
           <Col
             span={24}
             style={{
-              display: "flex",
-              alignItems: "center",
-              paddingLeft: "20px"
+              display: 'flex',
+              alignItems: 'center',
+              paddingLeft: '20px'
             }}
           >
-            <img src={logo} className="header-logo" alt="pmm-logo" />
+            <img src={logo} className='header-logo' alt='pmm-logo' />
             <p
               style={{
-                marginLeft: "10px",
-                marginRight: "10px",
-                fontSize: "20px"
+                marginLeft: '10px',
+                marginRight: '10px',
+                fontSize: '20px'
               }}
             >
               Percona Monitoring and Management
@@ -57,32 +57,36 @@ function App() {
           </Col>
         </Row>
       </div>
-      <div className="instance-id-pane">
-        <h2 className="instance-id-header">Instance ID verification</h2>
+      <div className='instance-id-pane'>
+        <h2 className='instance-id-header'>Instance ID verification</h2>
         <p>
           Please provide the Amazon Instance ID (AMI ID) from the AWS Console.
           <br />
           It uses the format of i-abc123def
         </p>
-        <p className="form-wrapper">
-          <p className="input-field-wrapper">
+        <p className='form-wrapper'>
+          <p className='input-field-wrapper'>
             <input
               onChange={e => setInstanceId(e.target.value)}
-              type="text"
-              placeholder="Instance ID"
-              className="instance-id-input-field"
+              type='text'
+              placeholder='Instance ID'
+              className='instance-id-input-field'
             />
             <Button
               onClick={checkInstance}
-              type="primary"
+              type='primary'
               loading={loading}
               disabled={loading}
-              className="instance-id-submit-button"
+              className='instance-id-submit-button'
             >
               Submit
             </Button>
           </p>
-          <a href="https://www.percona.com/doc/percona-monitoring-and-management/2.x/index.html#pmm-server-aws-running-instance">
+          <a
+            href='http://www.percona.com/doc/percona-monitoring-and-management/2.x/install/aws.html#pmm-server-aws-running-instance'
+            target='_blank'
+            rel='noopener noreferrer'
+          >
             Where should I get my instance ID?
           </a>
         </p>
