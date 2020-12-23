992e16d5b16e916b782173b4e989ae81cfa5056a
adivinho <vadim.yalovets@percona.com>
2020-12-23 17:54:36 +0200

    PMM-7085 DBaaS dashboard showing even when feature flag not enabled

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -o errexit
 # pmm-managed-init validates environment variables.
 pmm-managed-init
 
-/usr/share/pmm-server/import-dbass-dashboard.py&
+/usr/share/percona-dashboards/import-dbass-dashboard.py&
 
 # Start supervisor in foreground
 exec supervisord -n -c /etc/supervisord.conf
