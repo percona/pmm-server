d43c78ddec1397f765a0bde61fdb130df47a3160
adivinho <vadim.yalovets@percona.com>
2021-03-04 20:54:16 +0200

    PMM-5319 PoC : Home dashboards for PMM2

--- import-dashboards.py
+++ import-dashboards.py
@@ -474,9 +474,9 @@ def add_demo_footer():
 
 
 def set_home_dashboard(api_key):
-    # Get dashboard information by dashboard slug (name) which is "home-dashboard" in our case
+    # Get dashboard information by dashboard slug (name) which is "environments-overview" in our case
     # This API is different from /api/dashboards/home which returns home dashboard
-    r = requests.get('%s/api/dashboards/db/home-dashboard' % (HOST,), headers=grafana_headers(api_key))
+    r = requests.get('%s/api/dashboards/db/environments-overview' % (HOST,), headers=grafana_headers(api_key))
     print ' * PMM "home" dashboard: %r %r' % (r.status_code, r.content)
     if r.status_code != httplib.OK:
         # TODO sys.exit(-1)
