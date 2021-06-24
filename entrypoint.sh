15fc4b7b9ec37ac595676d9cbea7093179f5d0dd
Nikita Beletskii <nikita.beletskii@percona.com>
2021-06-24 19:07:04 +0300

    PMM-5847 move after pmm-managed-init

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,16 @@
 
 set -o errexit
 
+# pmm-managed-init validates environment variables.
+pmm-managed-init
+
 # export custom environment variables
-ENV_FILE=/srv/grafana/envs
+ENV_FILE=/srv/envvars
 if [[ -f "$ENV_FILE" ]]; then
     set -a # enable export for all variables
     source $ENV_FILE
     set +a
 fi
-
-# pmm-managed-init validates environment variables.
-pmm-managed-init
 
 # Start supervisor in foreground
 exec supervisord -n -c /etc/supervisord.conf
