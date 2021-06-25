7306f657b6f0462575ed16c4633d05b909d2a026
Nikita Beletskii <nikita.beletskii@percona.com>
2021-06-25 17:26:49 +0300

    PMM-5847 revert + add exceptions

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,38 @@
 
 set -o errexit
 
-# pmm-managed-init validates environment variables.
-pmm-managed-init
-
 # export custom environment variables
 ENV_FILE=/srv/envvars
+
+ENVS_EXCEPTION=('^GF_'
+                '^SUPERVISOR_'
+                '^PERCONA_TEST_'
+                '^DISABLE_UPDATES='
+                '^DISABLE_TELEMETRY='
+                '^METRICS_RESOLUTION='
+                '^METRICS_RESOLUTION_HR='
+                '^METRICS_RESOLUTION_MR='
+                '^METRICS_RESOLUTION_LR='
+                '^DATA_RETENTION='
+                '^ENABLE_VM_CACHE='
+                '^ENABLE_ALERTING='
+                '^ENABLE_AZUREDISCOVER='
+                '^ENABLE_BACKUP_MANAGEMENT='
+                '^ENABLE_DBAAS='
+                '^PMM_DEBUG='
+                '^PMM_TRACE='
+                '^HOME='
+                '^HOSTNAME='
+)
+
+GREP_EXPRESSION=`( IFS=$'|'; echo "${ENVS_EXCEPTION[*]}" )`
+
 if [[ -f "$ENV_FILE" ]]; then
-    set -a # enable export for all variables
-    source $ENV_FILE
-    set +a
+    export $(cat $ENV_FILE | egrep "$GREP_EXPRESSION" | xargs)
 fi
+
+# pmm-managed-init validates environment variables.
+pmm-managed-init
 
 # Start supervisor in foreground
 exec supervisord -n -c /etc/supervisord.conf
