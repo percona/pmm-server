98220c2e57a0159e340f155054d88412b9cbd50d
Nikita Beletskii <nikita.beletskii@percona.com>
2022-05-25 07:26:52 +0000

    PMM-8082 add srv initialization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
-
 set -o errexit
+
+# init /srv if empty
+DIST_FILE=/srv/pmm-distribution
+if [ ! -f $DIST_FILE ]; then
+    echo "File $DIST_FILE doesn't exist. Initizlize /srv..."
+    echo docker > $DIST_FILE
+    mkdir -p /srv/{clickhouse,grafana,logs,postgres,prometheus,nginx,victoriametrics}
+    chown grafana:grafana /srv/grafana
+    chown pmm:pmm /srv/{victoriametrics,prometheus,logs}
+    echo "Generate self-signed certificates for nginx"
+    bash /var/lib/cloud/scripts/per-boot/generate-ssl-certificate
+    echo "Init Postgres"
+    su postgres -c "/usr/pgsql-11/bin/initdb -D /srv/postgres"
+fi
 
 # pmm-managed-init validates environment variables.
 pmm-managed-init
